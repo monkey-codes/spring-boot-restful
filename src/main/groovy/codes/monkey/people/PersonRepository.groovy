d188cb4c86966cab3bb6ed5fbbd029951affa2b1
monkey codes <jz@monkey.codes>
2016-02-15 07:09:15 +0000

    Groovyfy

--- src/main/groovy/codes/monkey/people/PersonRepository.groovy
+++ src/main/groovy/codes/monkey/people/PersonRepository.groovy
@@ -4,8 +4,8 @@ import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@RepositoryRestResource(collectionResourceRel = 'people', path = 'people')
 interface PersonRepository extends MongoRepository<Person, String>{
 
-    List<Person> findByLastName(@Param("name") String name);
+    List<Person> findByLastName(@Param('name') String name)
 }
